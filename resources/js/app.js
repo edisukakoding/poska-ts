815db6504f12f266a549c489d9b2b6962ef7e1bf
Edi Hartono <edi.sukakoding@gmail.com>
2023-06-06 16:08:43 +0700

    init

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+// import '../css/app.css'
+
+// JUEASY UI
+import './../css/jeasyui/themes/material-teal/easyui.css'
+import './../css/jeasyui/themes/icon.css'
+// import './../css/jeasyui/desktop.css'
+import './jeasyui/jquery.min'
+import './jeasyui/jquery.easyui.min'
+import './jeasyui/jquery.desktop'
+// END JEASY UI
